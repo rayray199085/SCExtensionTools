10132d5370e92e07c47e34ddefbf255394cc0735
Rui Cao <rayray199085@gmail.com>
2019-04-22 10:40:09 +1000

    Add UIApplication request authorisation function

--- SCExtensionTools/SCExtensionTools/Tools/Network/SCNetworkManager.swift
+++ SCExtensionTools/SCExtensionTools/Tools/Network/SCNetworkManager.swift
@@ -55,7 +55,7 @@ class SCNetworkManager{
             completion(
                 dataResponse.result.value,
                 dataResponse.result.isSuccess,
-                dataResponse.response?.statusCode ?? -1,
+                dataResponse.response?.statusCode ?? 403,
                 dataResponse.result.error)
         }
     }
