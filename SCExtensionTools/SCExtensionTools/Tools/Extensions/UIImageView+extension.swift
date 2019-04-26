78496d2a65fb5b00290b0a7f884a6f2249c70bbb
Rui Cao <rayray199085@gmail.com>
2019-04-26 18:07:43 +1000

    Update setting circle image function

--- SCExtensionTools/SCExtensionTools/Tools/Extensions/UIImageView+extension.swift
+++ SCExtensionTools/SCExtensionTools/Tools/Extensions/UIImageView+extension.swift
@@ -15,17 +15,17 @@ extension UIImageView{
     ///   - urlString: image url string
     ///   - placeholderImage: placeholder
     ///   - isAvatar: ture if this image is used for avatar
-    func setImage(urlString: String?,placeholderImage: UIImage?, isAvatar:Bool = false){
+    func setImage(urlString: String?,backgroundColor: UIColor,placeholderImage: UIImage?, isAvatar:Bool = false, frameColor: UIColor = UIColor.lightGray){
         guard let imageUrlString = urlString,
             let url = URL(string: imageUrlString) else {
-            image = placeholderImage
-            return
+                image = placeholderImage
+                return
         }
         sd_setImage(with: url, placeholderImage: placeholderImage, options: [], progress: { (receivedSize, expectedSize, targetURL) in//NSInteger receivedSize, NSInteger expectedSize, NSURL * _Nullable targetURL);
-
+            
         }) { [weak self] (image, error, cacheType, imageURL) in//UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL
             if isAvatar{
-                 self?.image = image?.setCircleImage(size: self?.bounds.size, backgroundColor: UIColor.white, hasFrame: true, frameColor: UIColor.lightGray)
+                self?.image = image?.setCircleImage(size: self?.bounds.size, backgroundColor: backgroundColor, hasFrame: true, frameColor: frameColor)
             }else{
                 self?.image = image?.modifyImageSize(size: self?.bounds.size)
             }
