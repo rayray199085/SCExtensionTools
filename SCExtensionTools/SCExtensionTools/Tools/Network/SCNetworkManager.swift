d1cc0171d24d961e7d646d37f540c1304aed49df
Rui Cao <rayray199085@gmail.com>
2019-05-11 17:56:04 +1000

    Add network upload function

--- SCExtensionTools/SCExtensionTools/Tools/Network/SCNetworkManager.swift
+++ SCExtensionTools/SCExtensionTools/Tools/Network/SCNetworkManager.swift
@@ -11,7 +11,8 @@ import Alamofire
 
 class SCNetworkManager{
     static let shared = SCNetworkManager()
-    var accessToken: String? = "2.00aDhGmB0LD4Zk9321b34e6ayua33B"
+    var access_token: String?
+    
     private init() {
         
     }
@@ -22,10 +23,12 @@ class SCNetworkManager{
     ///   - urlString: url in string
     ///   - method: get/ post
     ///   - params: parameters in dictionary
+    ///   - name: name in server, like: "pic" for upload
+    ///   - data: file in data for upload
     ///   - completion: json(array/ dictionary), isSuccess
-    func requestWithToken(urlString: String,method: HTTPMethod,params:[String:Any]?, completion:@escaping (_ list:Any?, _ isSuccess: Bool)->()){
-        guard let token = accessToken else{
-            // FIXME: no token exists send notification
+    func requestWithToken(urlString: String,method: HTTPMethod,params:[String:Any]?, name: String? = nil, data: Data? = nil, completion:@escaping (_ list:Any?, _ isSuccess: Bool)->()){
+        guard let token = access_token else{
+            // send notification for relogin
             completion(nil,false)
             return
         }
@@ -34,13 +37,27 @@ class SCNetworkManager{
             params = [String:Any]()
         }
         params?["access_token"] = token
-        request(urlString: urlString, method: method, params: params) { (res, isSuccess, statusCode, _) in
-            if statusCode == 403 {
-                // FIXME: handle overdue token send notification
-                completion(nil,false)
-                return
+        
+        if let name = name,
+            let data = data{
+            // if name & data are not nil, use upload function
+            upload(urlString: urlString, name: name, params: params, data: data) { (res, isSuccess, statusCode, _) in
+                if statusCode == 403 {
+                    // send notification for relogin
+                    completion(nil,false)
+                    return
+                }
+                completion(res,isSuccess)
             }
-            completion(res,isSuccess)
+        }else{
+            request(urlString: urlString, method: method, params: params) { (res, isSuccess, statusCode, _) in
+                if statusCode == 403 {
+                   // send notification for relogin
+                    completion(nil,false)
+                    return
+                }
+                completion(res,isSuccess)
+            }
         }
     }
     /// request method
@@ -55,8 +72,45 @@ class SCNetworkManager{
             completion(
                 dataResponse.result.value,
                 dataResponse.result.isSuccess,
-                dataResponse.response?.statusCode ?? 403,
+                dataResponse.response?.statusCode ?? -1,
                 dataResponse.result.error)
+        }
+    }
+    
+    
+    /// Upload a file to server
+    ///
+    /// - Parameters:
+    ///   - urlString: url in string
+    ///   - name: name in server, like: "pic"
+    ///   - params: a dictionary for parameters
+    ///   - data: file in data
+    ///   - completion: response in json / nil
+    func upload(urlString: String, name: String, params: [String: Any]?,data: Data, completion:@escaping (_ response: Any?,_ isSuccess: Bool, _ statusCode: Int,_ error: Error?)->()){
+        Alamofire.upload(multipartFormData: { (multipartFormData) in
+            multipartFormData.append(
+                data,
+                withName: name,
+                fileName: "xxxxxx",
+                mimeType: "application/octet-stream")
+            for (key,value) in params ?? [:]{
+                multipartFormData.append(
+                    (value as! String).data(using: String.Encoding.utf8)!,
+                    withName: key)
+            }
+        }, to: urlString) { (encodingResult) in
+            switch encodingResult {
+            case .success(let upload, _, _):
+                upload.responseJSON { response in
+                    completion(
+                        response.result.value,
+                        response.result.isSuccess,
+                        response.response?.statusCode ?? -1,
+                        response.result.error)
+                }
+            case .failure(let encodingError):
+                completion(nil, false, -1, encodingError)
+            }
         }
     }
 }
